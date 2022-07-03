cd30b3ee729a3036997a0bc4f124582b83ed26b1
kimdat546 <kimdat546@gmail.com>
2022-07-04 00:24:15 +0700

    fix: req.body undefind

--- pages/api/check.js
+++ pages/api/check.js
@@ -3,9 +3,12 @@ import GmailAPI from "../../src/GmailAPI"
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      const response = await GmailAPI.readInboxContent(req.body.transid)
-      console.log(req.body.transid)
-      res.status(200).json({ result: response })
+      if (req?.body) {
+        const response = await GmailAPI.readInboxContent(req?.body?.transid)
+        console.log(req?.body?.transid)
+        res.status(200).json({ result: response })
+      }
+      res.status(50).json({ error: "req.body invalid" })
     } catch (error) {
       res.status(500).json({ error: `Internal server errors: ${error}` })
     }
